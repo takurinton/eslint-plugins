4763a3deebbcd1e05242731022d0728afb1725ea
takurinton <takurinton@takurinton.dev>
2024-08-18 23:25:43 +0900

    define

--- packages/i18n/src/index.ts
+++ packages/i18n/src/index.ts
@@ -1,14 +1,16 @@
-import { constantsRule } from "./rules";
+import { constantsRule, defineLanguageConstantVariables } from "./rules";
 
 export = {
   rules: {
     constants: constantsRule,
+    defineLanguageConstantVariables,
   },
   configs: {
     all: {
       plugins: ["i18n"],
       rules: {
         "i18n/constants": "error",
+        "i18n/define-language-constant-variables": "error",
       },
     },
   },
